c57833bec9421d0848137646076944f110cb5fd3
Adam Koprowski <koper@google.com>
2014-11-23 21:21:50 -0800

    Added 'About me' & application routing.

--- js/angular.js
+++ js/angular.js
@@ -1,7 +1,19 @@
-angular.module('homepage', [])
+angular.module('homepage', ['ngRoute', 'ngAnimate'])
+  .config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+      $routeProvider
+        .when('/main.html', {
+          templateUrl: 'main.html'
+        })
+        .when('/aboutme.html', {
+          templateUrl: 'aboutme.html'
+        })
+        .otherwise('/main.html');
+      $locationProvider.html5Mode(false);
+  }])
   .controller('NavCtrl', ['$scope', '$location', function($scope, $location) {
     $scope.menuItems = [
-      { href: 'index.html', icon: 'home', text: 'Main page' },
+      { href: 'main.html', icon: 'home', text: 'Main page' },
       { href: 'aboutme.html', icon: 'user', text: 'About me' },
       { href: 'publications.html', icon: 'edit', text: 'Publications' },
       { href: 'cv.pdf', icon: 'download-alt', text: 'Download CV' }
