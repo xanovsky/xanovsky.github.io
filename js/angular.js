6b54f2d45a789ea0199960f7330c8936727fa181
Adam Koprowski <koper@google.com>
2014-11-23 20:02:11 -0800

    Added option to download CV.

--- js/angular.js
+++ js/angular.js
@@ -3,7 +3,8 @@ angular.module('homepage', [])
     $scope.menuItems = [
       { href: 'index.html', icon: 'home', text: 'Main page' },
       { href: 'aboutme.html', icon: 'user', text: 'About me' },
-      { href: 'publications.html', icon: 'edit', text: 'Publications' }
+      { href: 'publications.html', icon: 'edit', text: 'Publications' },
+      { href: 'cv.pdf', icon: 'download-alt', text: 'Download CV' }
     ];
     $scope.isCurrent = function(menuItem) {
       return $location.absUrl().indexOf(menuItem.href) !== -1;
