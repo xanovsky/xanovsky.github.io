b73c83c0474b607f38ef2a402392acb8bb65692d
Adam Koprowski <koper@google.com>
2014-11-23 22:00:09 -0800

    Fixed link to cv.pdf & removed unused animation tags.

--- js/angular.js
+++ js/angular.js
@@ -8,14 +8,17 @@ angular.module('homepage', ['ngRoute', 'ngAnimate'])
         .when('/aboutme.html', {
           templateUrl: 'aboutme.html'
         })
+	.when('/cv.pdf', {
+          redirectTo: '/cv.pdf'
+        })
         .otherwise('/main.html');
       $locationProvider.html5Mode(false);
   }])
   .controller('NavCtrl', ['$scope', '$location', function($scope, $location) {
     $scope.menuItems = [
-      { href: 'main.html', icon: 'home', text: 'Main page' },
-      { href: 'aboutme.html', icon: 'user', text: 'About me' },
-      { href: 'publications.html', icon: 'edit', text: 'Publications' },
+      { href: '#/main.html', icon: 'home', text: 'Main page' },
+      { href: '#/aboutme.html', icon: 'user', text: 'About me' },
+      { href: '#/publications.html', icon: 'edit', text: 'Publications' },
       { href: 'cv.pdf', icon: 'download-alt', text: 'Download CV' }
     ];
     $scope.isCurrent = function(menuItem) {
