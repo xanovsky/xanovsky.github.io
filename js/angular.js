33711d8f21d6b75942d202adde5cada3a7f7936e
xanovsky <alexandra@asilva.cs.ucl.ac.uk>
2017-01-31 15:16:06 +0000

    bug?

--- js/angular.js
+++ js/angular.js
@@ -184,7 +184,7 @@ angular.module('homepage', ['ngRoute', 'ngAnimate', 'ngSanitize'])
       $location.hash(old);
     };
 
-    var affixOffset = $('.pageheader').height() + $('#filters').height() + 100 ;
+    var affixOffset = $('.pageheader').height() + $('#filters').height();
     $('#pub-year-nav').affix({
       offset: {
         top: affixOffset
